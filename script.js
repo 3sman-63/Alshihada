8fff29926c95574d313fe9a3571045ace813b79f
Osman Mohamed <100099949+3sman-63@users.noreply.github.com>
2024-08-19 17:18:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var article = document.querySelector( ".article" );
+
+var btn = document.querySelectorAll( ".btn" );
+
+var links = document.querySelector(".links")
+
+
+function active() {
+    article.classList.toggle( "activation" )
+    links.classList.toggle("dplaynone")
+}
